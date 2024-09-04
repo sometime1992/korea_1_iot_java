c113456f9455cb76652fb06f5372fb3b767f887d
sometime1992 <sometime1992@naver.com>
2024-09-04 15:52:53 +0900

    20240904

---
+++ java_study/src/chapter01/Z_Basic.java
@@ -0,0 +1,31 @@
+package chapter01;
+
+public class Z_Basic {
+
+	public static void main(String[] args) {
+		// 이클립스 단축키
+		// 단축키: crtl + /
+
+		// 한 줄 주석: 슬래시(/) 2개
+		// 여러 줄 주석
+		/*
+		 * 슬래시 2개 사이에 별(*) 두개
+		 */
+
+		// 줄 복사: ctrl + alt + 방향키(위/아래)
+		// 줄 이동: alt + 방향키
+		// 줄 정렬: ctrl + shift + f
+		// 줄 삭제: ctrl + d
+		
+		// 파일 저장: ctrl + s
+		// 전체 파일 저장: ctrl + shift + s
+		
+		// 파일 실행: ctrl + f11
+		
+		// 이전 상황 돌아가기: ctrl + z
+
+		// 안녕하세요
+
+	}
+
+}
